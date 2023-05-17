1aae5b1aab187f35c41b74ec785ae144d3e1f34c
namevug <ciavug@gmail.com>
2023-05-17 18:07:29 -0300

    feat: First commit

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+// HTTP Method: GET, POST, PUT, PATCH, DELETE
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+// API RESTful
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-2xl font-bold uppercase w-20 h-20 bg-red-500 rounded-xl m-10">
+      oi
+    </div>
+  )
+}
